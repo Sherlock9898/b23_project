eb4f7060699c56febd7e812bbf7ef17f1f01d9c1
Jakadef <jakadef507@gmail.com>
2023-05-11 09:28:34 +0500

    I am done with more work ready to push

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,15 @@ public class GitCool {
     // B23 IS LEARNING GIT
     // B23 IS A SPECIAL NUMBER
     // B23 IS MAKING SOME SENSE NOW
+
+    // We have added our Github account under intellIJ
+    // make sure to click apply and click on to save
+
+    // we were able to successufully shared project under our github account
+    // Share Project on GitHub happen only one time per project
+    // it combines the 3 actions below
+    //Create remote repository under your account
+    // Create the link between local repo to remote repo
+    // Push  local commits to remote repo
+
 }
